04dc1de0c77c9718bd57b19c4356391a99070525
Rocket47 <romtswork@gmail.com>
2019-02-11 21:43:59 +0300

    Added basic class for fragment.

--- app/src/main/java/com/example/myfirstapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myfirstapplication/ProfileActivity.java
@@ -1,7 +1,6 @@
 package com.example.myfirstapplication;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
